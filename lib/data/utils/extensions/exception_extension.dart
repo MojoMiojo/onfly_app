b56e490932ac97b821080a9131e960eb615bdf72
Mozart Sousa <mozartjunio9@gmail.com>
2023-10-18 21:26:31 -0300

    feat: print dioException

--- lib/data/utils/extensions/exception_extension.dart
+++ lib/data/utils/extensions/exception_extension.dart
@@ -1,16 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:onfly_app/data/utils/exceptions/map_value_exception.dart';
 import 'package:onfly_app/data/utils/exceptions/response_exception.dart';
 
 extension ExceptionExtension on Exception {
   void debugPrint() {
-    var msg = 'Unexpected excpetion';
+    var msg = 'Unexpected exception';
 
     if (this is MapValueException) {
       msg = "Fail to parse json";
     }
     if (this is ResponseException) {
       msg = "Received param isn't a Map<String, dynamic>";
+    }
+
+    if (this is DioException) {
+      msg = toString();
     }
 
     if (kDebugMode) {
